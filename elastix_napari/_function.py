0c9c9ad6f78236ac88ad06e1eebcc6c4bd747539
ViktorvdValk <viktorvandervalk@gmail.com>
2021-03-18 18:32:57 +0100

    ENH:Layout_and_function_update

--- elastix_napari/_function.py
+++ elastix_napari/_function.py
@@ -12,23 +12,43 @@ from enum import Enum
 import numpy as np
 from napari_plugin_engine import napari_hook_implementation
 from magicgui import magic_factory
-from itk import itkElastixRegistrationMethodPython
 import itk
+from pathlib import Path
+from typing import Sequence
 
 # if TYPE_CHECKING:
 # from napari.types import ImageData, LabelsData, LayerDataTuple
+def on_init(widget):
+    widget.native.setStyleSheet("QWidget{font-size: 12pt;}")
 
-@magic_factory(call_button="register", transform = {"choices": ["rigid", "affine", "bspline"]})
-def register(fixed: 'napari.types.ImageData', moving: 'napari.types.ImageData', transform: str) -> 'napari.types.LayerDataTuple':
+@magic_factory(widget_init=on_init, layout='vertical', call_button="register",
+                transform = {"choices": ["rigid", "affine", "bspline"]},
+                filenames={"label":"parameterfile (optional):", "filter":"*.txt"})
+def register(fixed: 'napari.types.ImageData', moving: 'napari.types.ImageData', transform: str, filenames: Sequence[Path]) -> 'napari.types.LayerDataTuple':
+    if fixed is None or moving is None:
+        print("No images selected for registration.")
+        return
+    fixed = np.asarray(fixed).astype(np.float32)
+    moving = np.asarray(moving).astype(np.float32)
     parameter_object = itk.ParameterObject.New()
-    default_rigid_parameter_map = parameter_object.GetDefaultParameterMap(transform, 3)
-    parameter_object.AddParameterMap(default_rigid_parameter_map)
+    print(type(filenames[0]))
+    print(filenames[0])
+    filename = str(filenames[0])
+    if ".txt" in filename:
+        transform = 'custom'
+        try:
+            parameter_object.AddParameterFile(filename)
+        except:
+            print("Parameter file not found or not valid.")
+    else:
+        default_rigid_parameter_map = parameter_object.GetDefaultParameterMap(transform, 3)
+        parameter_object.AddParameterMap(default_rigid_parameter_map)
     result_image, result_transform_parameters = itk.elastix_registration_method(
         fixed, moving,
         parameter_object=parameter_object,
-        log_to_console=False)
+        log_to_console=True)
     return np.asarray(result_image).astype(np.float32), {'name':transform + ' Registration'}
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return register
+    return register, {'area': 'bottom'}
