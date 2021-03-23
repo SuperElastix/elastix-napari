2d40b6330304a808384b367ca3a00bc71d5c05f1
ViktorvdValk <viktorvandervalk@gmail.com>
2021-03-23 16:24:31 +0100

    ENH:AddVersionFunction_FixRequirements

--- elastix_napari/__init__.py
+++ elastix_napari/__init__.py
@@ -3,9 +3,9 @@
 __author__ = "Viktor van der Valk"
 __email__ = "v.o.van_der_valk@lumc.nl"
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 def get_module_version():
     return __version__
-    
+
 from .elastix_registration import napari_experimental_provide_dock_widget
