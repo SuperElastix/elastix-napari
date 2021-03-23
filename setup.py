e476ea1a934e295c3333234d1e3da4cadacde72b
ViktorvdValk <viktorvandervalk@gmail.com>
2021-03-23 17:34:15 +0100

    CI:Disable_Tagless_Updates

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@
 import os
 import codecs
 from setuptools import setup, find_packages
-from elastix_napari import __version__
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
@@ -36,7 +35,7 @@ setup(
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=requirements,
-    version = __version__,
+    version = "0.0.5",
     setup_requires=['setuptools_scm'],
     classifiers=[
         'Development Status :: 4 - Beta',
