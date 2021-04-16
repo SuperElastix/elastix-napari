d13a4c0eff617dd3c7d700ddcc46d2d58c3c2a07
Viktor van der Valk <33719474+ViktorvdValk@users.noreply.github.com>
2021-04-16 09:08:02 +0200

    Advanced options: Support for point set registration, initial transforms and proper error pop ups (#8)
    * ENH:Point_set_registration
    
    * ENH:InitialTransform
    
    * ENH:PointsetMetricCoupled
    
    * ENH:Error_pop_ups_and_GUI_update
    
    * ENH:Version to 0.1.3

--- elastix_napari/elastix_registration.py
+++ elastix_napari/elastix_registration.py
@@ -15,55 +15,136 @@ from magicgui import magic_factory
 import itk
 from pathlib import Path
 from typing import Sequence
+from qtpy.QtWidgets import QMessageBox
+
+
+def error(message):
+    e = QMessageBox()
+    label = QMessageBox()
+    e.setText(message)
+    e.setIcon(QMessageBox.Critical)
+    e.setWindowTitle("Error")
+    e.show()
+    return e
+
+
+def check_pointset(pointset):
+    if '.txt' in str(pointset[0]) or '.vtk' in str(pointset[0]):
+        return True
+    else:
+        return False
 
 
 def on_init(widget):
     widget.native.setStyleSheet("QWidget{font-size: 12pt;}")
 
-    widget.fixed_mask.visible = False
-    widget.moving_mask.visible = False
-    widget.param1.visible = False
-    widget.param2.visible = False
-    widget.param3.visible = False
-
+    for x in ['fixed_mask', 'moving_mask', 'param1', 'param2', 'param3',
+              'fixed_ps', 'moving_ps', 'metric', 'init_trans',
+              'resolutions', 'max_iterations', 'nr_spatial_samples',
+              'max_step_length']:
+        setattr(getattr(widget, x), 'visible', False)
 
     def toggle_mask_widgets(event):
-        widget.fixed_mask.visible = event.value
-        widget.moving_mask.visible = event.value
+        for x in ['fixed_mask', 'moving_mask']:
+            setattr(getattr(widget, x), 'visible', event.value)
 
     def toggle_preset_widget(event):
         if event.value == "custom":
-            widget.param1.visible = True
-            widget.param2.visible = True
-            widget.param3.visible = True
+            for x in ['param1', 'param2', 'param3']:
+                setattr(getattr(widget, x), 'visible', True)
+            for y in ['metric', 'resolutions', 'max_iterations',
+                      'nr_spatial_samples', 'max_step_length']:
+                setattr(getattr(widget, y), 'visible', False)
+
         else:
-            widget.param1.visible = False
-            widget.param2.visible = False
-            widget.param3.visible = False
+            for x in ['param1', 'param2', 'param3']:
+                setattr(getattr(widget, x), 'visible', False)
+            for x in ['metric', 'init_trans', 'resolutions', 'max_iterations',
+                      'nr_spatial_samples', 'max_step_length', 'moving_ps',
+                      'fixed_ps']:
+                setattr(getattr(widget, x), 'visible', widget.advanced.value)
+
+    def toggle_advanced_widget(event):
+        if widget.preset.value == "custom":
+            for x in ['init_trans', 'fixed_ps', 'moving_ps']:
+                setattr(getattr(widget, x), 'visible', event.value)
+        else:
+            for x in ['metric', 'init_trans', 'resolutions',
+                      'max_iterations', 'nr_spatial_samples',
+                      'max_step_length', 'fixed_ps', 'moving_ps']:
+                setattr(getattr(widget, x), 'visible', event.value)
 
     widget.preset.changed.connect(toggle_preset_widget)
-    widget.use_masks.changed.connect(toggle_mask_widgets)
+    widget.masks.changed.connect(toggle_mask_widgets)
+    widget.advanced.changed.connect(toggle_advanced_widget)
     widget.native.layout().addStretch()
 
 
 @magic_factory(widget_init=on_init, layout='vertical', call_button="register",
-               preset={"choices": ["rigid", "affine", "bspline", "custom"]},
-
-               param1={"label": "parameterfile:",
-               "filter": "*.txt"}, param2={"label": "parameterfile 2",
-               "filter": "*.txt"}, param3={"label": "parameterfile 3",
-               "filter": "*.txt"})
+               preset={"choices": ["rigid", "affine", "bspline", "custom"],
+                       "tooltip": "Select a preset parameter file or select "
+                       "custom to load a custom one"},
+               fixed_ps={"label": "fixed point set", "filter": "*.txt",
+                         "tooltip": "Load a fixed point set"},
+               moving_ps={"label": "moving point set", "filter": "*.txt",
+                          "tooltip": "Load a moving point set"},
+               param1={"label": "parameterfile 1", "filter": "*.txt",
+                       "tooltip": 'Load a custom parameter file'},
+               param2={"label": "parameterfile 2", "filter": "*.txt",
+                       "tooltip": 'Optionally load a second custom parameter '
+                       'file'},
+               param3={"label": "parameterfile 3", "filter": "*.txt",
+                       "tooltip": 'Optionally load a third custom parameter '
+                       'file'},
+               metric={"choices": ["from preset",
+                                   "AdvancedMattesMutualInformation",
+                                   "AdvancedNormalizedCorrelation",
+                                   "AdvancedMeanSquares"],
+                       "tooltip": 'Select a metric to use'},
+               init_trans={"label": "initial transform", "filter": "*.txt",
+                           "tooltip": 'Load a initial transform from a .txt '
+                           'file'},
+               nr_spatial_samples={"max": 8192, "step": 256,
+                                   "tooltip": 'Select the number of spatial '
+                                   'samples to use'})
 def elastix_registration(fixed: 'napari.types.ImageData',
-                         moving: 'napari.types.ImageData',
+                         moving: 'napari.types.ImageData', preset: str,
                          fixed_mask: 'napari.types.ImageData',
-                         moving_mask: 'napari.types.ImageData', preset: str,
+                         moving_mask: 'napari.types.ImageData',
+                         fixed_ps: Sequence[Path], moving_ps: Sequence[Path],
                          param1: Sequence[Path], param2: Sequence[Path],
-                         param3: Sequence[Path], use_masks: bool = False
+                         param3: Sequence[Path], init_trans: Sequence[Path],
+                         metric: str, resolutions: int = 4,
+                         max_iterations: int = 500,
+                         nr_spatial_samples: int = 512,
+                         max_step_length: float = 1.0,  masks: bool = False,
+                         advanced: bool = False
+                         ) -> 'napari.types.LayerDataTuple':
 
-                         )-> 'napari.types.LayerDataTuple':
     if fixed is None or moving is None:
         print("No images selected for registration.")
-        return
+        return error("No images selected for registration.")
+    if check_pointset(fixed_ps) != check_pointset(moving_ps):
+        print("Select both fixed and moving point set.")
+        return error("Select both fixed and moving point set.")
+
+    if advanced:
+        if check_pointset(init_trans):
+            init_trans = str(init_trans[0])
+        else:
+            init_trans = ''
+        if check_pointset(fixed_ps):
+            fixed_ps = str(fixed_ps[0])
+        else:
+            fixed_ps = ''
+        if check_pointset(moving_ps):
+            moving_ps = str(moving_ps[0])
+        else:
+            moving_ps = ''
+    else:
+        init_trans = ''
+        fixed_ps = ''
+        moving_ps = ''
 
     # Casting to numpy is currently necessary
     # because of napari's type ambiguity.
@@ -78,56 +159,82 @@ def elastix_registration(fixed: 'napari.types.ImageData',
                 try:
                     parameter_object.AddParameterFile(par)
                 except:
-                    raise TypeError("Parameter file not found or not valid")
+                    return error("Parameter file not found or not valid")
             else:
                 pass
     else:
-        default_parameter_map = parameter_object.GetDefaultParameterMap(preset)
-        parameter_object.AddParameterMap(default_parameter_map)
+        if advanced:
+            parameter_map = \
+                parameter_object.GetDefaultParameterMap(preset, resolutions)
+            if metric != 'from preset':
+                parameter_map['Metric'] = [metric]
+            if fixed_ps != '' and moving_ps != '':
+                parameter_map['Registration'] = [
+                    'MultiMetricMultiResolutionRegistration']
+                original_metric = parameter_map['Metric']
+                parameter_map['Metric'] = \
+                    [original_metric[0],
+                        'CorrespondingPointsEuclideanDistanceMetric']
+            parameter_map['MaximumStepLength'] = [str(max_step_length)]
+            parameter_map['NumberOfSpatialSamples'] = [str(nr_spatial_samples)]
+            parameter_map['MaximumNumberOfIterations'] = [str(max_iterations)]
+        else:
+            parameter_map = parameter_object.GetDefaultParameterMap(preset)
+        parameter_object.AddParameterMap(parameter_map)
 
-    if use_masks:
+    if not masks:
+        result_image, result_transform_parameters = \
+            itk.elastix_registration_method(
+                fixed, moving, parameter_object,
+                fixed_point_set_file_name=fixed_ps,
+                moving_point_set_file_name=moving_ps,
+                initial_transform_parameter_file_name=init_trans,
+                log_to_console=True)
+
+    elif masks:
         if fixed_mask is None and moving_mask is None:
             print("No masks selected for registration")
-            return
+            return error("No masks selected for registration")
         else:
             # Casting to numpy and itk is currently necessary
             # because of napari's type ambiguity.
 
-            if not (fixed_mask is None):
+            if moving_mask is None:
                 fixed_mask = np.asarray(fixed_mask).astype(np.uint8)
                 fixed_mask = itk.image_view_from_array(fixed_mask)
+                result_image, rtp = itk.elastix_registration_method(
+                    fixed, moving, parameter_object, fixed_mask=fixed_mask,
+                    fixed_point_set_file_name=fixed_ps,
+                    moving_point_set_file_name=moving_ps,
+                    initial_transform_parameter_file_name=init_trans,
+                    log_to_console=False)
 
-                if not (moving_mask is None):
-                    moving_mask = np.asarray(moving_mask).astype(np.uint8)
-                    moving_mask = itk.image_view_from_array(moving_mask)
-
-                    result_image, result_transform_parameters = \
-                        itk.elastix_registration_method(
-                            fixed, moving, parameter_object,
-                            fixed_mask=fixed_mask,moving_mask=moving_mask,
-                            log_to_console=False)
-                else:
-                    result_image, result_transform_parameters = \
-                        itk.elastix_registration_method(
-                            fixed, moving, parameter_object,
-                            fixed_mask=fixed_mask,log_to_console=False)
+            elif fixed_mask is None:
+                moving_mask = np.asarray(moving_mask).astype(np.uint8)
+                moving_mask = itk.image_view_from_array(moving_mask)
+                result_image, rtp = itk.elastix_registration_method(
+                    fixed, moving, parameter_object, moving_mask=moving_mask,
+                    fixed_point_set_file_name=fixed_ps,
+                    moving_point_set_file_name=moving_ps,
+                    initial_transform_parameter_file_name=init_trans,
+                    log_to_console=False)
             else:
-                if not (moving_mask is None):
-                    moving_mask = np.asarray(moving_mask).astype(np.uint8)
-                    moving_mask = itk.image_view_from_array(moving_mask)
+                fixed_mask = np.asarray(fixed_mask).astype(np.uint8)
+                fixed_mask = itk.image_view_from_array(fixed_mask)
+                moving_mask = np.asarray(moving_mask).astype(np.uint8)
+                moving_mask = itk.image_view_from_array(moving_mask)
 
-                    result_image, result_transform_parameters = \
-                        itk.elastix_registration_method(
-                            fixed, moving, parameter_object,
-                            moving_mask=moving_mask, log_to_console=False)
+                result_image, rtp = itk.elastix_registration_method(
+                    fixed, moving, parameter_object, fixed_mask=fixed_mask,
+                    moving_mask=moving_mask,
+                    fixed_point_set_file_name=fixed_ps,
+                    moving_point_set_file_name=moving_ps,
+                    initial_transform_parameter_file_name=init_trans,
+                    log_to_console=False)
 
-    else:
-        result_image, result_transform_parameters = \
-            itk.elastix_registration_method(fixed, moving, parameter_object,
-                                            log_to_console=False)
-    return np.asarray(result_image).astype(np.float32), {'name':preset + ' Registration'}
+    return np.asarray(result_image).astype(np.float32), {'name': preset + ' Registration'}
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return elastix_registration, {'area': 'bottom'}
+    return elastix_registration
--- elastix_napari/tests/test_registration.py
+++ elastix_napari/tests/test_registration.py
@@ -3,15 +3,30 @@ import elastix_napari
 import itk
 from elastix_napari import elastix_registration
 import numpy as np
+from qtpy.QtWidgets import QMessageBox
+
 
 # Test widget function
 def test_dock_widget():
-    assert elastix_napari.napari_experimental_provide_dock_widget() != None
+    assert elastix_napari.napari_experimental_provide_dock_widget() is not None
+
 
 # Helper functions
-def image_generator(x1, x2, y1, y2, mask=False, artefact=False):
+def image_generator(x1, x2, y1, y2, mask=False, artefact=False,
+                    pointset=False, ps_name='fixed', data_dir='none'):
     if mask:
         image = np.zeros([100, 100], np.uint8)
+    elif pointset:
+        # Create fixed point set
+        filename = ps_name + "_point_set_test.txt"
+        point_set = open(str(data_dir / filename), "w+")
+        point_set.write("point\n4\n")
+        point_set.write(str(x1) + " " + str(y1) + "\n")
+        point_set.write(str(x1) + " " + str(y2) + "\n")
+        point_set.write(str(x2) + " " + str(y1) + "\n")
+        point_set.write(str(x2) + " " + str(y2) + "\n")
+        point_set.close()
+        return (str(data_dir / filename), 'x')
     else:
         image = np.zeros([100, 100], np.float32)
     image[y1:y2, x1:x2] = 1
@@ -20,17 +35,20 @@ def image_generator(x1, x2, y1, y2, mask=False, artefact=False):
     image = itk.image_view_from_array(image)
     return image
 
+
 def get_er(*args, **kwargs):
     er_func = elastix_registration.elastix_registration()
-    return er_func(*args,**kwargs)
+    return er_func(*args, **kwargs)
+
 
 # Test normal registration
 def test_registration():
-    fixed_image = image_generator(25,75,25,75)
-    moving_image = image_generator(1,51,10,60)
+    fixed_image = image_generator(25, 75, 25, 75)
+    moving_image = image_generator(1, 51, 10, 60)
     result_image = get_er(fixed_image, moving_image, preset='rigid')[0]
     mean_diff = np.absolute(np.subtract(result_image, fixed_image)).mean()
     assert mean_diff < 0.001
+
 
 # Test Masked registration
 def test_masked_registration():
@@ -43,36 +61,72 @@ def test_masked_registration():
 
     result_image = get_er(fixed=fixed_image, moving=moving_image,
                           fixed_mask=fixed_mask, moving_mask=moving_mask,
-                          preset='rigid', use_masks=True)[0]
+                          preset='rigid', masks=True)[0]
 
     # Filter artifacts out of the images.
-    masked_fixed_image = np.asarray(fixed_image)[0:90,0:90]
+    masked_fixed_image = np.asarray(fixed_image)[0:90, 0:90]
     masked_result_image = result_image[0:90, 0:90]
 
     mean_diff = np.absolute(np.subtract(masked_fixed_image,
                                         masked_result_image)).mean()
     assert mean_diff < 0.001
 
+
+# Test Point set registration
+def test_pointset_registration(data_dir):
+    fixed_image = image_generator(25, 75, 25, 75)
+    moving_image = image_generator(1, 51, 10, 60)
+
+    # Create pointsets for artefact
+    fixed_ps = image_generator(25, 75, 25, 75, pointset=True, ps_name='fixed',
+                               data_dir=data_dir)
+    moving_ps = image_generator(1, 51, 10, 60, pointset=True, ps_name='moving',
+                                data_dir=data_dir)
+
+    result_image = get_er(fixed_image, moving_image, fixed_ps=fixed_ps,
+                          moving_ps=moving_ps, preset='rigid',
+                          metric='from preset', advanced=True)[0]
+
+    mean_diff = np.absolute(np.subtract(result_image, fixed_image)).mean()
+    assert mean_diff < 0.001
+
+
+# Test registration with custom parameter textfiles
 def test_custom_registration(data_dir):
-    fixed_image = image_generator(25,75,25,75)
-    moving_image = image_generator(1,51,10,60)
+    fixed_image = image_generator(25, 75, 25, 75)
+    moving_image = image_generator(1, 51, 10, 60)
 
     filename = "parameters_Rigid.txt"
     result_image = get_er(fixed_image, moving_image, preset='custom',
-                          param1=(str(data_dir / filename),'x'), param2=(str(data_dir / filename), 'x'))[0]
+                          param1=(str(data_dir / filename), 'x'),
+                          param2=(str(data_dir / filename), 'x'))[0]
 
     mean_diff = np.absolute(np.subtract(result_image, fixed_image)).mean()
-    assert mean_diff < 0.1
+    assert mean_diff < 0.01
+
+
+def test_initial_transform(data_dir):
+    fixed_image = image_generator(25, 75, 25, 75)
+    moving_image = image_generator(1, 51, 10, 60)
+
+    init_trans_filename = "TransformParameters.0.txt"
+    result_image = get_er(
+        fixed_image, moving_image, preset='rigid',
+        init_trans=(str(data_dir / init_trans_filename), 'x'),
+        metric='from preset', resolutions=6, max_iterations=500,
+        advanced=True)[0]
+    mean_diff = np.absolute(np.subtract(result_image, fixed_image)).mean()
+    assert mean_diff < 0.01
+
 
 def test_empty_images():
     im = get_er(None, None, preset='rigid')
-    assert im == None
+    assert isinstance(im, QMessageBox)
+
 
 def test_empty_masks():
-    fixed_image = image_generator(25,75,25,75)
-    moving_image = image_generator(1,51,10,60)
-    im = get_er(fixed_image, moving_image, fixed_mask=None, moving_mask=None, preset='rigid', use_masks=True)
-    assert im == None
-# print(test_registration())
-# print(test_custom_registration(data_dir))
-# print(elastix_registration.elastix_registration()())
+    fixed_image = image_generator(25, 75, 25, 75)
+    moving_image = image_generator(1, 51, 10, 60)
+    im = get_er(fixed_image, moving_image, fixed_mask=None, moving_mask=None,
+                preset='rigid', masks=True)
+    assert isinstance(im, QMessageBox)
