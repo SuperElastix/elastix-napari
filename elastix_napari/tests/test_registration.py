e208044bb773d5cdbf154cee0c5655074a162858
ViktorvdValk <viktorvandervalk@gmail.com>
2021-05-10 17:24:11 +0200

    ENH:AdvancedMattesMutualInformation_as_default_metric

--- elastix_napari/tests/test_registration.py
+++ elastix_napari/tests/test_registration.py
@@ -85,7 +85,7 @@ def test_pointset_registration(data_dir):
 
     result_image = get_er(fixed_image, moving_image, fixed_ps=fixed_ps,
                           moving_ps=moving_ps, preset='rigid',
-                          metric='from preset', advanced=True)[0]
+                          advanced=True)[0]
 
     mean_diff = np.absolute(np.subtract(result_image, fixed_image)).mean()
     assert mean_diff < 0.001
@@ -112,9 +112,8 @@ def test_initial_transform(data_dir):
     init_trans_filename = "TransformParameters.0.txt"
     result_image = get_er(
         fixed_image, moving_image, preset='rigid',
-        init_trans=(str(data_dir / init_trans_filename), 'x'),
-        metric='from preset', resolutions=6, max_iterations=500,
-        advanced=True)[0]
+        init_trans=(str(data_dir / init_trans_filename), 'x'), resolutions=6,
+        max_iterations=500, advanced=True)[0]
     mean_diff = np.absolute(np.subtract(result_image, fixed_image)).mean()
     assert mean_diff < 0.01
 
