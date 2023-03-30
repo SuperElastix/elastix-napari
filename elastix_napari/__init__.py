3b93631ddc9d58a40dd0af5f5574192ac199ff09
Konstantinos Ntatsis <konstantinos.ntatsis323@gmail.com>
2023-03-30 17:57:34 +0200

    ENH: Bump version to 0.1.8

--- elastix_napari/__init__.py
+++ elastix_napari/__init__.py
@@ -3,7 +3,7 @@ from .elastix_registration import napari_experimental_provide_dock_widget
 __author__ = "Viktor van der Valk"
 __email__ = "v.o.van_der_valk@lumc.nl"
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 
 def get_module_version():
