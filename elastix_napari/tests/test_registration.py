490afab92a1ba3d66c3bc52a6873616fd3fa70a1
Konstantinos Ntatsis <konstantinos.ntatsis323@gmail.com>
2023-05-12 16:17:18 +0200

    ENH: Use elastix backend for test assertions

--- elastix_napari/tests/test_registration.py
+++ elastix_napari/tests/test_registration.py
@@ -16,66 +16,84 @@ def get_er(*args, **kwargs):
 
 # Test normal registration
 # @pytest.mark.parametrize("fixed_image, moving_image", [(fixed_image_2D, moving_image_2D),])
-def test_registration(fixed_image, moving_image):
+def test_registration(fixed_image, moving_image, default_rigid):
     result_image = get_er(fixed_image, moving_image, preset='rigid')
-    mean_diff = np.absolute(np.subtract(
-        np.asarray(image_from_image_layer(result_image)),
-        np.asarray(image_from_image_layer(fixed_image)))).mean()
-    assert mean_diff < 0.001
+
+    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
+                                                             image_from_image_layer(moving_image), 
+                                                             parameter_object=default_rigid)
+    assert np.allclose(image_from_image_layer(result_image), reference_result_image)
 
 
 # Test Masked registration
-def test_masked_registration(fixed_image, moving_image, fixed_mask, moving_mask):
+def test_masked_registration(fixed_image, moving_image, fixed_mask, moving_mask, default_rigid):
     result_image = get_er(fixed_image, moving_image,
                           fixed_mask=fixed_mask, moving_mask=moving_mask,
                           preset='rigid', masks=True)
+    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
+                                                             image_from_image_layer(moving_image),
+                                                             fixed_mask=image_from_image_layer(fixed_mask),
+                                                             moving_mask=image_from_image_layer(moving_mask),
+                                                             parameter_object=default_rigid)
 
-    # Filter artifacts out of the images.
-    masked_fixed_image = np.asarray(
-        image_from_image_layer(fixed_image))[0:90, 0:90]
-    masked_result_image = np.asarray(
-        image_from_image_layer(result_image))[0:90, 0:90]
-
-    mean_diff = np.absolute(np.subtract(masked_fixed_image,
-                                        masked_result_image)).mean()
-    assert mean_diff < 0.001
+    assert np.allclose(image_from_image_layer(result_image), reference_result_image)
 
 
 # Test Point set registration
-def test_pointset_registration(fixed_image, moving_image, fixed_ps, moving_ps):
+def test_pointset_registration(fixed_image, moving_image, fixed_ps, moving_ps, default_rigid):
     result_image = get_er(fixed_image, moving_image, fixed_ps=fixed_ps,
                           moving_ps=moving_ps, preset='rigid',
                           advanced=True)
+    default_rigid.SetParameter(0, "Registration", "MultiMetricMultiResolutionRegistration")
+    default_rigid.SetParameter(0, "Metric", [default_rigid.GetParameter(0, "Metric")[0], 
+                                             "CorrespondingPointsEuclideanDistanceMetric"])
+    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
+                                                             image_from_image_layer(moving_image),
+                                                             parameter_object=default_rigid,
+                                                             fixed_point_set_file_name=str(fixed_ps),
+                                                             moving_point_set_file_name=str(moving_ps))
+    assert np.allclose(image_from_image_layer(result_image), reference_result_image, atol=0.5)
 
-    mean_diff = np.absolute(np.subtract(
-        np.asarray(image_from_image_layer(result_image)),
-        np.asarray(image_from_image_layer(fixed_image)))).mean()
-    assert mean_diff < 0.001
 
 
 # Test registration with custom parameter textfiles
+# TODO: Test multiple parameter files as well
 def test_custom_registration(fixed_image, moving_image, data_dir):
     filename = "parameters_Rigid.txt"
     result_image = get_er(fixed_image, moving_image, preset='custom',
-                          param1=data_dir / filename,
-                          param2=data_dir / filename)
+                          param1=data_dir / filename)
 
-    mean_diff = np.absolute(np.subtract(
-        np.asarray(image_from_image_layer(result_image)),
-        np.asarray(image_from_image_layer(fixed_image)))).mean()
-    assert mean_diff < 0.01
+    parameter_object = itk.ParameterObject.New()
+    parameter_object.AddParameterFile(str(data_dir / filename))
+
+    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
+                                                             image_from_image_layer(moving_image),
+                                                             parameter_object=parameter_object)
+    assert np.allclose(image_from_image_layer(result_image), reference_result_image)
 
 
-def test_initial_transform(fixed_image, moving_image, data_dir):
+# TODO: This test tests more than one things --> split into two
+# TODO: Test that any combination of UI options is depicted correctly in the parameter object
+def test_initial_transform(fixed_image, moving_image, default_rigid, data_dir):
     init_trans_filename = "TransformParameters.0.txt"
+    resolutions = 3
+    max_iterations = 50
     result_image = get_er(
         fixed_image, moving_image, preset='rigid',
-        init_trans=data_dir / init_trans_filename, resolutions=6,
-        max_iterations=500, advanced=True)
-    mean_diff = np.absolute(np.subtract(
-        np.asarray(image_from_image_layer(result_image)),
-        np.asarray(image_from_image_layer(fixed_image)))).mean()
-    assert mean_diff < 0.01
+        init_trans=data_dir / init_trans_filename, resolutions=resolutions,
+        max_iterations=max_iterations, advanced=True)
+        
+    # TODO: Advanced should take default values from the current parameter map
+    default_rigid.SetParameter(0, "Metric", "AdvancedMattesMutualInformation")
+    default_rigid.SetParameter(0, "MaximumNumberOfIterations", str(max_iterations))
+    default_rigid.SetParameter(0, "NumberOfResolutions", str(resolutions))
+    default_rigid.SetParameter(0, "NumberOfSpatialSamples", str(512))
+    default_rigid.SetParameter(0, "MaximumStepLength", str(1.0))
+
+    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
+                                                             image_from_image_layer(moving_image), 
+                                                             parameter_object=default_rigid)
+    assert np.allclose(image_from_image_layer(result_image), reference_result_image)
 
 
 def test_empty_images():
@@ -98,5 +116,3 @@ def test_writing_result(fixed_image, moving_image, tmpdir):
                 output_dir=tmpdir)
     assert (tmpdir / "TransformParameters.0.txt").exists()
     
-
-
