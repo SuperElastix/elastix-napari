467f8017a7932d5ff5b4caf397c8f392b31b4bc8
Niels Dekker <N.Dekker@lumc.nl>
2025-10-27 15:42:49 +0100

    STYLE: Reformat test code by running `python -m black *.py`
    Follow-up to pull request https://github.com/SuperElastix/elastix-napari/pull/47 commit 0ad507fbec38889c5a56677bbc6e2f4d1feaf624 "STYLE: Reformat code by running `python -m black *.py`"

--- elastix_napari/tests/conftest.py
+++ elastix_napari/tests/conftest.py
@@ -11,23 +11,23 @@ if TYPE_CHECKING:
     import napari
 
 
-
-
 ############################################################################################
 # Implementation from: https://github.com/pytest-dev/pytest/issues/3730#issuecomment-567142496
 # Code to remove items from the cartesian product of fixtures e.g. [images_2D, masks_3D]
 def pytest_configure(config):
     config.addinivalue_line(
-        "markers", "uncollect_if(*, func): function to unselect tests from parametrization"
+        "markers",
+        "uncollect_if(*, func): function to unselect tests from parametrization",
     )
+
 
 def pytest_collection_modifyitems(config, items):
     removed = []
     kept = []
     for item in items:
-        m = item.get_closest_marker('uncollect_if')
+        m = item.get_closest_marker("uncollect_if")
         if m:
-            func = m.kwargs['func']
+            func = m.kwargs["func"]
             if func(**item.callspec.params):
                 removed.append(item)
                 continue
@@ -35,24 +35,27 @@ def pytest_collection_modifyitems(config, items):
     if removed:
         config.hook.pytest_deselected(items=removed)
         items[:] = kept
-############################################################################################
 
+
+############################################################################################
 
 
 @pytest.fixture(scope="session")
 def data_dir() -> Path:
     return Path(__file__).parent / "data"
 
+
 @pytest.fixture(scope="session")
-def images_2D(data_dir) -> Tuple['napari.layers.Image', 'napari.layers.Image']:
+def images_2D(data_dir) -> Tuple["napari.layers.Image", "napari.layers.Image"]:
     fixed_image = itk.imread(str(data_dir / "CT_2D_head_fixed.mha"), itk.F)
     fixed_image = image_layer_from_image(fixed_image)
     moving_image = itk.imread(str(data_dir / "CT_2D_head_moving.mha"), itk.F)
     moving_image = image_layer_from_image(moving_image)
     return (fixed_image, moving_image)
 
+
 @pytest.fixture(scope="session")
-def images_3D(data_dir) -> Tuple['napari.layers.Image', 'napari.layers.Image']:
+def images_3D(data_dir) -> Tuple["napari.layers.Image", "napari.layers.Image"]:
     # Create a stack of 2D arrays to use as 3D image
     def create_3D_image(filepath):
         image = itk.imread(filepath, itk.F)
@@ -61,52 +64,64 @@ def images_3D(data_dir) -> Tuple['napari.layers.Image', 'napari.layers.Image']:
         image = itk.image_from_array(image)
         image = image_layer_from_image(image)
         return image
-    
+
     fixed_image = create_3D_image(str(data_dir / "CT_2D_head_fixed.mha"))
     moving_image = create_3D_image(str(data_dir / "CT_2D_head_moving.mha"))
     return (fixed_image, moving_image)
+
 
 @pytest.fixture(scope="session", params=["images_2D", "images_3D"])
 def images(request):
     return request.getfixturevalue(request.param)
 
-#TODO: Make more meaningful masks
+
+# TODO: Make more meaningful masks
 @pytest.fixture(scope="session")
-def masks_2D() -> Tuple['napari.layers.Image', 'napari.layers.Image']:
+def masks_2D() -> Tuple["napari.layers.Image", "napari.layers.Image"]:
     mask = np.zeros([100, 100], np.uint8)
     mask[:90, :100] = 1
     mask = itk.image_view_from_array(mask)
-    mask = image_layer_from_image(mask)    
+    mask = image_layer_from_image(mask)
     return (mask, mask)
 
+
 @pytest.fixture(scope="session")
-def masks_3D() -> Tuple['napari.layers.Image', 'napari.layers.Image']:
+def masks_3D() -> Tuple["napari.layers.Image", "napari.layers.Image"]:
     mask = np.zeros([10, 100, 100], np.uint8)
     mask[:, :90, :100] = 1
     mask = itk.image_view_from_array(mask)
-    mask = image_layer_from_image(mask)    
+    mask = image_layer_from_image(mask)
     return (mask, mask)
+
 
 @pytest.fixture(scope="session", params=["masks_2D", "masks_3D"])
 def masks(request):
     return request.getfixturevalue(request.param)
 
-@pytest.fixture(scope="session")
-def pointsets_2D(data_dir) -> Tuple['Path', 'Path']:
-    return (data_dir / "fixed_pointset_2D_test.txt", 
-            data_dir / "moving_pointset_2D_test.txt")
 
 @pytest.fixture(scope="session")
-def pointsets_3D(data_dir) -> Tuple['Path', 'Path']:
-    return (data_dir / "fixed_pointset_3D_test.txt", 
-            data_dir / "moving_pointset_3D_test.txt")
+def pointsets_2D(data_dir) -> Tuple["Path", "Path"]:
+    return (
+        data_dir / "fixed_pointset_2D_test.txt",
+        data_dir / "moving_pointset_2D_test.txt",
+    )
+
+
+@pytest.fixture(scope="session")
+def pointsets_3D(data_dir) -> Tuple["Path", "Path"]:
+    return (
+        data_dir / "fixed_pointset_3D_test.txt",
+        data_dir / "moving_pointset_3D_test.txt",
+    )
+
 
 @pytest.fixture(scope="session", params=["pointsets_2D", "pointsets_3D"])
 def pointsets(request):
     return request.getfixturevalue(request.param)
 
+
 @pytest.fixture(scope="function")
-def default_rigid() -> 'itk.ParameterObject':
+def default_rigid() -> "itk.ParameterObject":
     default_rigid = itk.ParameterObject.New()
-    default_rigid.SetParameterMap(default_rigid.GetDefaultParameterMap('rigid'))
+    default_rigid.SetParameterMap(default_rigid.GetDefaultParameterMap("rigid"))
     return default_rigid
--- elastix_napari/tests/test_registration.py
+++ elastix_napari/tests/test_registration.py
@@ -13,22 +13,25 @@ def get_er(*args, **kwargs):
     er_func = elastix_registration.elastix_registration()
     return er_func(*args, **kwargs)
 
+
 # See explanation in conftest.py
 def uncollect_if(images=None, masks=None, pointsets=None):
     if not pointsets:
-        return ('2D' in images) != ('2D' in masks)
+        return ("2D" in images) != ("2D" in masks)
     else:
-        return ('2D' in images) != ('2D' in pointsets)
+        return ("2D" in images) != ("2D" in pointsets)
 
 
 def test_registration(images, default_rigid, data_dir):
     fixed_image, moving_image = images
-    result_image = get_er(fixed_image, moving_image, preset='rigid')
+    result_image = get_er(fixed_image, moving_image, preset="rigid")
 
-    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
-                                                             image_from_image_layer(moving_image), 
-                                                             parameter_object=default_rigid)
-    
+    reference_result_image, _ = itk.elastix_registration_method(
+        image_from_image_layer(fixed_image),
+        image_from_image_layer(moving_image),
+        parameter_object=default_rigid,
+    )
+
     fixed_filepath = Path(data_dir) / "fixed.nii"
     moving_filepath = Path(data_dir) / "moving.nii"
 
@@ -39,19 +42,27 @@ def test_registration(images, default_rigid, data_dir):
     itk.imwrite(moving_image, str(moving_filepath))
     assert np.allclose(image_from_image_layer(result_image), reference_result_image)
 
+
 # Test masked registration
 @pytest.mark.uncollect_if(func=uncollect_if)
 def test_masked_registration(images, masks, default_rigid):
     fixed_image, moving_image = images
     fixed_mask, moving_mask = masks
-    result_image = get_er(fixed_image, moving_image,
-                          fixed_mask=fixed_mask, moving_mask=moving_mask,
-                          preset='rigid', masks=True)
-    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
-                                                             image_from_image_layer(moving_image),
-                                                             fixed_mask=image_from_image_layer(fixed_mask),
-                                                             moving_mask=image_from_image_layer(moving_mask),
-                                                             parameter_object=default_rigid)
+    result_image = get_er(
+        fixed_image,
+        moving_image,
+        fixed_mask=fixed_mask,
+        moving_mask=moving_mask,
+        preset="rigid",
+        masks=True,
+    )
+    reference_result_image, _ = itk.elastix_registration_method(
+        image_from_image_layer(fixed_image),
+        image_from_image_layer(moving_image),
+        fixed_mask=image_from_image_layer(fixed_mask),
+        moving_mask=image_from_image_layer(moving_mask),
+        parameter_object=default_rigid,
+    )
 
     assert np.allclose(image_from_image_layer(result_image), reference_result_image)
 
@@ -61,19 +72,35 @@ def test_masked_registration(images, masks, default_rigid):
 def test_pointset_registration(images, pointsets, default_rigid):
     fixed_image, moving_image = images
     fixed_point_set, moving_point_set = pointsets
-    result_image = get_er(fixed_image, moving_image, fixed_point_set=fixed_point_set,
-                          moving_point_set=moving_point_set, preset='rigid',
-                          advanced=True)
-    default_rigid.SetParameter(0, "Registration", "MultiMetricMultiResolutionRegistration")
-    default_rigid.SetParameter(0, "Metric", [default_rigid.GetParameter(0, "Metric")[0], 
-                                             "CorrespondingPointsEuclideanDistanceMetric"])
-    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
-                                                             image_from_image_layer(moving_image),
-                                                             parameter_object=default_rigid,
-                                                             fixed_point_set_file_name=str(fixed_point_set),
-                                                             moving_point_set_file_name=str(moving_point_set))
-    assert np.allclose(image_from_image_layer(result_image), reference_result_image, atol=0.5)
-
+    result_image = get_er(
+        fixed_image,
+        moving_image,
+        fixed_point_set=fixed_point_set,
+        moving_point_set=moving_point_set,
+        preset="rigid",
+        advanced=True,
+    )
+    default_rigid.SetParameter(
+        0, "Registration", "MultiMetricMultiResolutionRegistration"
+    )
+    default_rigid.SetParameter(
+        0,
+        "Metric",
+        [
+            default_rigid.GetParameter(0, "Metric")[0],
+            "CorrespondingPointsEuclideanDistanceMetric",
+        ],
+    )
+    reference_result_image, _ = itk.elastix_registration_method(
+        image_from_image_layer(fixed_image),
+        image_from_image_layer(moving_image),
+        parameter_object=default_rigid,
+        fixed_point_set_file_name=str(fixed_point_set),
+        moving_point_set_file_name=str(moving_point_set),
+    )
+    assert np.allclose(
+        image_from_image_layer(result_image), reference_result_image, atol=0.5
+    )
 
 
 # Test registration with custom parameter textfiles
@@ -81,15 +108,18 @@ def test_pointset_registration(images, pointsets, default_rigid):
 def test_custom_registration(images, data_dir):
     fixed_image, moving_image = images
     filename = "parameters_Rigid.txt"
-    result_image = get_er(fixed_image, moving_image, preset='custom',
-                          parameterfile_1=data_dir / filename)
+    result_image = get_er(
+        fixed_image, moving_image, preset="custom", parameterfile_1=data_dir / filename
+    )
 
     parameter_object = itk.ParameterObject.New()
     parameter_object.AddParameterFile(str(data_dir / filename))
 
-    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
-                                                             image_from_image_layer(moving_image),
-                                                             parameter_object=parameter_object)
+    reference_result_image, _ = itk.elastix_registration_method(
+        image_from_image_layer(fixed_image),
+        image_from_image_layer(moving_image),
+        parameter_object=parameter_object,
+    )
     assert np.allclose(image_from_image_layer(result_image), reference_result_image)
 
 
@@ -105,10 +135,15 @@ def test_initial_transform(images, default_rigid, data_dir):
     resolutions = 3
     max_iterations = 50
     result_image = get_er(
-        fixed_image, moving_image, preset='rigid',
-        initial_transform=data_dir / initial_transform_filename, resolutions=resolutions,
-        max_iterations=max_iterations, advanced=True)
-        
+        fixed_image,
+        moving_image,
+        preset="rigid",
+        initial_transform=data_dir / initial_transform_filename,
+        resolutions=resolutions,
+        max_iterations=max_iterations,
+        advanced=True,
+    )
+
     # TODO: Advanced should take default values from the current parameter map
     default_rigid.SetParameter(0, "Metric", "AdvancedMattesMutualInformation")
     default_rigid.SetParameter(0, "MaximumNumberOfIterations", str(max_iterations))
@@ -116,36 +151,52 @@ def test_initial_transform(images, default_rigid, data_dir):
     default_rigid.SetParameter(0, "NumberOfSpatialSamples", str(512))
     default_rigid.SetParameter(0, "MaximumStepLength", str(1.0))
 
-    reference_result_image, _ = itk.elastix_registration_method(image_from_image_layer(fixed_image), 
-                                                             image_from_image_layer(moving_image), 
-                                                             parameter_object=default_rigid,
-                                                             initial_transform_parameter_file_name=str(data_dir / initial_transform_filename))
-    
+    reference_result_image, _ = itk.elastix_registration_method(
+        image_from_image_layer(fixed_image),
+        image_from_image_layer(moving_image),
+        parameter_object=default_rigid,
+        initial_transform_parameter_file_name=str(
+            data_dir / initial_transform_filename
+        ),
+    )
+
     diff = image_from_image_layer(result_image)[:] - reference_result_image[:]
     print(diff.min(), diff.max())
     assert np.allclose(image_from_image_layer(result_image), reference_result_image)
 
 
 def test_empty_images():
-    im = get_er(None, None, preset='rigid')
+    im = get_er(None, None, preset="rigid")
     assert isinstance(im, QMessageBox)
 
 
 def test_empty_masks(images):
     fixed_image, moving_image = images
-    im = get_er(fixed_image, moving_image, fixed_mask=None, moving_mask=None,
-                preset='rigid', masks=True)
+    im = get_er(
+        fixed_image,
+        moving_image,
+        fixed_mask=None,
+        moving_mask=None,
+        preset="rigid",
+        masks=True,
+    )
     assert isinstance(im, QMessageBox)
+
 
 def test_empty_output_directory(images):
     fixed_image, moving_image = images
-    im = get_er(fixed_image, moving_image, preset='rigid', save_output=True)
+    im = get_er(fixed_image, moving_image, preset="rigid", save_output=True)
     assert isinstance(im, QMessageBox)
+
 
 def test_writing_result(images, tmpdir):
     fixed_image, moving_image = images
     tmpdir = Path(tmpdir)
-    im = get_er(fixed_image, moving_image, preset='rigid', save_output=True,
-                output_directory=tmpdir)
+    im = get_er(
+        fixed_image,
+        moving_image,
+        preset="rigid",
+        save_output=True,
+        output_directory=tmpdir,
+    )
     assert (tmpdir / "TransformParameters.0.txt").exists()
-    
