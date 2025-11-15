917024b965acf48170cf9cbf7e33d042b39a6ffd
Niels Dekker <N.Dekker@lumc.nl>
2025-11-15 13:08:22 +0100

    ENH: Support napari layers of corresponding points
    Allowed the user to specify corresponding points by Points layers.

--- elastix_napari/elastix_registration.py
+++ elastix_napari/elastix_registration.py
@@ -3,20 +3,16 @@ import elastix_napari.utils as utils
 from magicgui import magic_factory
 import itk
 from pathlib import Path
-from itk_napari_conversion import image_from_image_layer
-from itk_napari_conversion import image_layer_from_image
+from itk_napari_conversion import (
+    image_from_image_layer,
+    image_layer_from_image,
+    point_set_from_points_layer,
+)
 
 # For IDE type support and autocompletion
 # https://napari.org/stable/plugins/building_a_plugin/best_practices.html#don-t-require-napari-if-not-necessary
 if TYPE_CHECKING:
     import napari
-
-
-def convert_file_path_to_string(file_path: Path) -> str:
-    """
-    Works around the problem that `str(file_path)` returns "." when the Path object is empty.
-    """
-    return "" if file_path == Path() else str(file_path)
 
 
 def on_init(widget):
@@ -32,7 +28,9 @@ def on_init(widget):
         "parameterfile_1",
         "parameterfile_2",
         "parameterfile_3",
+        "fixed_points",
         "fixed_point_set",
+        "moving_points",
         "moving_point_set",
         "metric",
         "resolutions",
@@ -75,10 +73,21 @@ def on_init(widget):
     @widget.use_corresponding_points.changed.connect
     def on_use_corresponding_points_changed(value):
         for name in [
-            "fixed_point_set",
-            "moving_point_set",
+            "fixed_points",
+            "moving_points",
         ]:
             getattr(widget, name).visible = value
+
+        widget.fixed_point_set.visible = value and widget.fixed_points.value is None
+        widget.moving_point_set.visible = value and widget.moving_points.value is None
+
+    @widget.fixed_points.changed.connect
+    def on_fixed_points_changed(value):
+        widget.fixed_point_set.visible = value is None
+
+    @widget.moving_points.changed.connect
+    def on_moving_points_changed(value):
+        widget.moving_point_set.visible = value is None
 
     @widget.advanced.changed.connect
     def on_advanced_changed(value):
@@ -162,7 +171,9 @@ def elastix_registration(
     log_to_file: bool = False,
     output_directory: Path = "",
     use_corresponding_points: bool = False,
+    fixed_points: "napari.layers.Points" = None,
     fixed_point_set: Path = "",
+    moving_points: "napari.layers.Points" = None,
     moving_point_set: Path = "",
     initial_transform: Path = "",
     advanced: bool = False,
@@ -177,13 +188,6 @@ def elastix_registration(
     """
     if fixed_image is None or moving_image is None:
         return utils.error("No images selected for registration.")
-    if fixed_point_set.exists() != moving_point_set.exists():
-        return utils.error("Select both fixed and moving point set.")
-
-    if not utils.check_filename(fixed_point_set):
-        fixed_point_set = ""
-    if not utils.check_filename(moving_point_set):
-        moving_point_set = ""
 
     # Convert image layer to itk_image
     fixed_image = image_from_image_layer(fixed_image)
@@ -192,6 +196,15 @@ def elastix_registration(
     moving_image = moving_image.astype(itk.F)
 
     parameter_object = itk.ParameterObject.New()
+
+    kwargs = {
+        "parameter_object": parameter_object,
+        "log_to_console": True,
+    }
+
+    if initial_transform != Path():
+        kwargs["initial_transform_parameter_file_name"] = str(initial_transform)
+
     if preset == "custom":
         for file_path in [parameterfile_1, parameterfile_2, parameterfile_3]:
             if file_path != Path():
@@ -212,25 +225,42 @@ def elastix_registration(
             parameter_map = parameter_object.GetDefaultParameterMap(preset, 4)
 
         if use_corresponding_points:
-            if str(fixed_point_set) == "" or str(moving_point_set) == "":
-                return utils.error("Please specify both point sets!")
-
             parameter_map["Registration"] = ["MultiMetricMultiResolutionRegistration"]
             parameter_map["Metric"] += ("CorrespondingPointsEuclideanDistanceMetric",)
+
+            if fixed_points is None:
+                if fixed_point_set == Path():
+                    return utils.error("Please specify the fixed points!")
+                else:
+                    kwargs["fixed_point_set_file_name"] = str(fixed_point_set)
+            else:
+                if fixed_points.data.size > 0:
+                    kwargs["fixed_points"] = point_set_from_points_layer(
+                        fixed_points
+                    ).GetPoints()
+                else:
+                    return utils.error(
+                        "Please make sure the selected layer of fixed points has one or more points!"
+                    )
+
+            if moving_points is None:
+                if moving_point_set == Path():
+                    return utils.error("Please specify the moving points!")
+                else:
+                    kwargs["moving_point_set_file_name"] = str(moving_point_set)
+            else:
+                if moving_points.data.size > 0:
+                    kwargs["moving_points"] = point_set_from_points_layer(
+                        moving_points
+                    ).GetPoints()
+                else:
+                    return utils.error(
+                        "Please make sure the selected layer of moving points has one or more points!"
+                    )
 
         parameter_object.AddParameterMap(parameter_map)
 
     args = [fixed_image, moving_image]
-
-    kwargs = {
-        "parameter_object": parameter_object,
-        "fixed_point_set_file_name": str(fixed_point_set),
-        "moving_point_set_file_name": str(moving_point_set),
-        "initial_transform_parameter_file_name": convert_file_path_to_string(
-            initial_transform
-        ),
-        "log_to_console": True,
-    }
 
     if use_masks:
         if fixed_mask is None and moving_mask is None:
--- elastix_napari/utils.py
+++ elastix_napari/utils.py
@@ -12,13 +12,3 @@ def error(message):
     e.setWindowTitle("Error")
     e.show()
     return e
-
-
-def check_filename(filename):
-    """
-    Checks if filename adheres to the correct format.
-    """
-    if (filename.suffix == ".txt") or (filename.suffix == ".vtk"):
-        return True
-    else:
-        return False
