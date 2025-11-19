080f89903144ce4cec87b62f7d27b81ae1fdaef2
Niels Dekker <N.Dekker@lumc.nl>
2025-11-19 14:40:09 +0100

    BUG: Fix `utils.error(message)` EmitLoopError and AttributeError
    The original code triggered an error, saying:
    
    > EmitLoopError:
    >
    > While emitting signal 'magicgui.widgets.PushButton.changed', an error occurred in a callback:
    >
    >  AttributeError: 'QMessageBox' object has no attribute 'source'
    
    Fixed by calling QMessageBox.critical, and returning nothing, instead of returning the created instance of QMessageBox.

--- elastix_napari/utils.py
+++ elastix_napari/utils.py
@@ -5,13 +5,8 @@ def error(message):
     """
     Shows a pop up with the given error message.
     """
-    e = QMessageBox()
     print("ERROR: ", message)
-    e.setText(message)
-    e.setIcon(QMessageBox.Critical)
-    e.setWindowTitle("Error")
-    e.show()
-    return e
+    QMessageBox.critical(None, "Error", message)
 
 
 def check_filename(filename):
