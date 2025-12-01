51e206ff90dc4899c4762957796d1e34704bf916
Niels Dekker <N.Dekker@lumc.nl>
2025-12-01 15:01:32 +0100

    STYLE: Remove `utils.check_filename`
    `utils.check_filename` was only used to check if the user has specified point set files. Currently, the plugin only supports elastix/transformix specific text files (*.txt).
    
    Note that `Path` parameters have default value "", whereas the emptiness of such a parameter must be checked by comparing the parameter to `Path()`, which represents the current directory, "."!

--- elastix_napari/utils.py
+++ elastix_napari/utils.py
@@ -7,13 +7,3 @@ def error(message):
     """
     print("ERROR: ", message)
     QMessageBox.critical(None, "Error", message)
-
-
-def check_filename(filename):
-    """
-    Checks if filename adheres to the correct format.
-    """
-    if (filename.suffix == ".txt") or (filename.suffix == ".vtk"):
-        return True
-    else:
-        return False
